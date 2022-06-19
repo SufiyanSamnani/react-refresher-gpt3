a32c0a13fa574aec37c1189f895017bce13c8764
Sufiyan Samnani <sufi.samnani@gmail.com>
2022-06-19 16:42:44 -0600

    Completed brands section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -5,12 +5,27 @@ Folder names for component should be lowercase.
 Each component needs its own CSS file, lowercase name */
 
 import React from 'react'
-import './brand.css' // use CSS file for this Brand component
+import './brand.css' // Use CSS file for this Brand component
+import { google, slack, atlassian, dropbox, shopify } from './imports' // Format not needed here
 
 const Brand = () => {
   return (
-    <div>
-        Brand
+    <div className='gpt3__brand section__padding'>
+        <div>
+            <img src={google} alt='google' />
+        </div>
+        <div>
+            <img src={slack} alt='slack' />
+        </div>
+        <div>
+            <img src={atlassian} alt='atlassian' />
+        </div>
+        <div>
+            <img src={dropbox} alt='dropbox' />
+        </div>
+        <div>
+            <img src={shopify} alt='shopify' />
+        </div>
     </div>
   )
 }
---
+++ src/components/brand/imports.js
@@ -0,0 +1,15 @@
+/* Helper file for all brand images */
+
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+export { // Exporting all images for brands to be used in Brand component
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify
+}
